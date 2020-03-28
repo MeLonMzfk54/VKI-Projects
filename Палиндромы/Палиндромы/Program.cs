6c833679ef4ae1c75758739b08ffe4ca85b6ff9e
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-03-28 20:42:54 +0700

    Лаба Палиндромы

---
+++ Палиндромы/Палиндромы/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Палиндромы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = true;
+            int number;
+            int choose;
+            do
+            {
+                Console.WriteLine("Введите число, чтобы проверить является ли оно палиндромом");
+                number = Int32.Parse(Console.ReadLine());
+                number.Pal();
+                Console.WriteLine("Продолжим?\n 1 - да \t 2 - нет");
+                choose = Int32.Parse(Console.ReadLine());
+                if(choose == 1)
+                {
+                    end = true;
+                }else if(choose == 2)
+                {
+                    end = false;
+                }
+            } while (end);
+        }
+    }
+}
---
+++ Палиндромы/Палиндромы/palindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Палиндромы
+{
+   static class palindrome
+    {
+        public static void Pal(this int numb)
+        {
+            bool check = true;
+            int view = numb;
+            int len = numb.ToString().Length;
+            int len1 = len;
+            for (int i = 1; i <= len / 2; i++)
+            {
+                if (numb % 10 != numb / (int)Math.Pow(10, len1 - 1))
+                {
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    check = true;
+                    len1 -= 1;
+                    numb/= 10;
+                    numb %= (int)Math.Pow(10, len1 - 1);
+                    len1 = numb.ToString().Length;
+                }
+            }
+
+            if (check == true)
+            {
+                Console.WriteLine(view + " - палиндром");
+            }
+            else
+            {
+                Console.WriteLine(view + " - не палиндром");
+            }
+        }
+    }
+}

53ec485cd4c2cad6d4c054a93166bc19ee729c6c
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-03-28 19:58:07 +0700

    Лаба делегаты

---
+++ Делегаты/Делегаты/Methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Делегаты
+{
+    class Methods
+    {
+        public double Add(double x)
+        {
+            double answer = x + 5;
+            Console.WriteLine("Операция сложения:" + answer);
+            return answer;
+        }
+        public double Multiply(double x)
+        {
+            double answer = x * 7;
+            Console.WriteLine("Операция умножения:" + answer);
+            return answer;
+        }
+        public double Subtraction(double x)
+        {
+            double answer = x - 3;
+            Console.WriteLine("Операция вычитания:" + answer);
+            return answer;
+        }
+        public double Division(double x)
+        {
+            double answer = x / 3;
+            Console.WriteLine("Операция деления без остатка:" + answer);
+            return answer;
+        }
+        public double Pow(double x)
+        {
+            double answer = x * x;
+            Console.WriteLine("Операция квадрата:" + answer);
+            return answer;
+        }
+    }
+}
---
+++ Делегаты/Делегаты/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Делегаты
+{
+    class Program
+    {
+       
+        
+            private delegate double DelegateM(double x);
+        static void Main(string[] args)
+        {
+            DelegateM[] arr = new DelegateM[10];
+            Random rnd = new Random();
+            Methods methods = new Methods();
+            for (int i = 0; i < 10; i++)
+            {
+                int a = Convert.ToInt32(rnd.Next(1, 5));
+                switch (a)
+                {
+                    case 1:
+                        arr[i] = methods.Add;
+                        break;
+                    case 2:
+                        arr[i] = methods.Multiply;
+                        break;
+                    case 3:
+                        arr[i] = methods.Division;
+                        break;
+                    case 4:
+                        arr[i] = methods.Subtraction;
+                        break;
+                    case 5:
+                        arr[i] = methods.Pow;
+                        break;
+
+                }
+            }
+            double num;
+            Console.WriteLine("Введите число, над которым хотите проделать операции");
+            num = double.Parse(Console.ReadLine());
+            for (int j = 0; j < 10; j++)
+            {
+                num = arr[j](num);
+
+            }
+
+        }
+    }
+}
+    
+

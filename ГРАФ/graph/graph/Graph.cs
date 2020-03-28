69ab6d63fc855e956299b78013fea55588300596
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-03-28 17:24:37 +0700

    Лаба Кортежи

---
+++ ГРАФ/graph/graph/Graph.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace graph
+{
+    class Graph
+    {
+        public HashSet<string> allCities(Tuple<string, string, int>[] tupleArray)
+        {
+            HashSet<string> cities = new HashSet<string>();
+            foreach ( var city in tupleArray)
+            {
+                cities.Add(city.Item1);
+            }
+            return cities;
+        }
+
+        public void sumRoads(Tuple<string, string, int>[] tupleArray)
+        {
+            int sum = 0;
+            foreach(var i in tupleArray)
+            {
+                sum += i.Item3;
+            }
+            Console.WriteLine("Сумма всех дорог: "+ sum);
+        }
+
+        public void closestCities(Tuple<string, string, int>[] tupleArray)
+        {
+            int min = tupleArray[0].Item3;
+            foreach(var i in tupleArray)
+            {
+                if(min >= i.Item3)
+                {
+                    min = i.Item3;
+                }
+            }
+            foreach(var i in tupleArray)
+            {
+                if(min == i.Item3)
+                {
+                    Console.WriteLine("Ближайшие города - " + i.Item1 + " и " + i.Item2);
+                }
+            }
+        }
+
+
+        public void MaxWeight(Tuple<string, string, int>[] tupleArray, HashSet<string> cities)            
+        {
+            string s;
+            int a = 0;
+            int sum = 0;
+            int road = 0;
+            string сity1 = "";
+            string сity2 = "";
+            foreach (var k in cities)
+            {
+                sum = 0;
+                s = k;
+                for (int i = 0; i < 3; i++)
+                {
+                    var per = Search(tupleArray, s, a);
+                    s = per.Item2;
+                    a = per.Item1;
+                    sum += per.Item1;
+                }
+                if (sum > road)
+                {
+                    road = sum;
+                    сity1 = k;
+                    сity2 = s;
+                }
+            }
+            Console.WriteLine("Самый длинных путь между " + сity1 + " и " + сity2 + " равен " + sum);
+        }
+
+        public (int, string, string) Search(Tuple<string, string, int>[] tupleArray, string city, int c)             
+        {
+            string str = "";
+            int max = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if (tupleArray[i].Item1 == city)
+                {
+                    if (tupleArray[i].Item3 > max && tupleArray[i].Item3 != c)
+                    {
+                        str = tupleArray[i].Item2;
+                        max = tupleArray[i].Item3;
+                    }
+                }
+            }
+            return (max, str, city);
+        }
+    }
+}
---
+++ ГРАФ/graph/graph/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuple<string, string, int>[] tupleArray =
+          {
+                Tuple.Create("A","B",5),
+                Tuple.Create("A","D",9),
+                Tuple.Create("B","A",5),
+                Tuple.Create("B","E",9),
+                Tuple.Create("C","D",7),
+                Tuple.Create("C","E",8),
+                Tuple.Create("D","A",9),
+                Tuple.Create("D","C",7),
+                Tuple.Create("D","E",6),
+                Tuple.Create("E","B",9),
+                Tuple.Create("E","C",8),
+                Tuple.Create("E","D",6)
+            };
+            Graph graph = new Graph();
+            HashSet<string> cities1 = graph.allCities(tupleArray);
+            Console.WriteLine("Список всех городов: ");
+           foreach(var i in cities1)
+            {
+                Console.Write("\t"+i);
+            }
+            Console.WriteLine();
+            graph.sumRoads(tupleArray);
+            graph.closestCities(tupleArray);
+            graph.MaxWeight(tupleArray,cities1);
+        }
+    }
+}

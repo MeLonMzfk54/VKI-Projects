8f4e9f851f5567eecc8910f204578df8ffa025d8
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-03-28 17:26:22 +0700

    Лаба события

---
+++ События/События/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace События
+{
+    class Message
+    {
+        public void txtRecord()
+        {
+            using (StreamWriter sw = new StreamWriter(@"zapis.txt", true, System.Text.Encoding.Default))
+            {
+                sw.Write("ку!");
+            }
+        }
+    }
+}
---
+++ События/События/Robot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace События
+{
+    class Robot
+    {
+
+        public delegate void MethodContainer();
+        public  event  MethodContainer onBack;
+        public void robotMove()
+        {
+            Thread robotThread = new Thread(move);
+            robotThread.Start();
+        }
+
+        public void move()
+        {
+            Random rnd = new Random();
+            int randomValue;
+            Thread.Sleep(100);
+            for(int i = 0; i < 10; i++)
+            {
+                randomValue = rnd.Next(1, 4);
+                switch (randomValue)
+                {
+                    case 1:
+                        Console.WriteLine("Робот идёт вперёд");
+                        break;
+                    case 2:
+                        Console.WriteLine("Робот идёт назад");
+                        onBack();
+                        break;
+                    case 3:
+                        Console.WriteLine("Робот идёт влево");
+                        break;
+                    case 4:
+                        Console.WriteLine("Робот идёт вправо");
+                        break;
+                    default:
+                        Console.WriteLine("Робот неисправен, гг!");
+                        break;
+                }
+            }
+        }
+
+    }
+}

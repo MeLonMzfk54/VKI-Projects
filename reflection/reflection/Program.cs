da3af6059b7029bf359a638aba45ce2d3b30d748
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-05-14 18:26:04 +0700

    Лаба рефлексия
    Лаба на рефлексию

---
+++ reflection/reflection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+namespace reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly ass = Assembly.LoadFrom("reflection"); 
+            Console.WriteLine("Полное имя сборки - "+ass.FullName+"\n"); 
+            Type[] myType = ass.GetTypes();
+            foreach (Type val in myType)
+            {
+                Console.WriteLine("Название - "+val.Name + "\n\nМетоды: ");
+                MethodInfo[] Minfo = val.GetMethods();
+                foreach (MethodInfo i in Minfo)
+                    Console.Write(i.ReturnType.Name + " => " + i.Name + "\n");
+            }
+        }
+    }
+}
